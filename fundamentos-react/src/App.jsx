25f094d49f9a61e044bdf44627dc0acfc64793fe
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2022-05-04 08:56:15 -0300

    Fundamentos React

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,12 +14,22 @@ import ParOuImpar from './components/condicional/ParOuImpar'
 import UsuarioInfo from './components/condicional/UsuarioInfo'
 import DiretaPai from './components/comunicacao/DiretaPai'
 import IndiretaPai from './components/comunicacao/IndiretaPai'
+import Input from './components/formulario/Input'
+import Contador from './components/contador/Contador'
 
 export default () => {
     return (
         <div className="App">
             <h1>Fundamentos React</h1>
             <div className="Cards">
+
+                <Card title="#12 - Contador" color="#424242">
+                    <Contador numeroInicial={10} />
+                </Card>
+
+                <Card title="#11 - Componente Controlado (input)" color="#ef45f6">
+                    <Input/>
+                </Card>
 
                 <Card title="#10 - Comunicação Indireta" color="#8bad39">
                     <IndiretaPai></IndiretaPai>

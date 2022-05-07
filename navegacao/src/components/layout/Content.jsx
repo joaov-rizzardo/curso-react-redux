3a4d03302d75ed6f2faa2641e622112443dad2df
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2022-05-07 15:17:00 -0300

    Navegação

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Content.css'
+import {Routes, Route} from 'react-router-dom'
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
+
+
+const Content = props => {
+    return(
+        <main className="Content">
+            <Routes>
+                <Route exact path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/param/:id" element={<Param/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </main>
+    )
+}
+
+export default Content

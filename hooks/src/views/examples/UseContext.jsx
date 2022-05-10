28bf55de081fdc9bbd854f59d44abc62bf982b81
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2022-05-10 08:47:54 -0300

    Hooks

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,8 +1,9 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 import DataContext from '../../data/DataContext'
-
+import {AppContext} from '../../data/Store'
 const UseContext = (props) => {
 
     const context = useContext(DataContext)
@@ -14,13 +15,20 @@ const UseContext = (props) => {
         })
     }
 
+    const {number, setNumber, text, setText} = useContext(AppContext)
+
+    useEffect(() => {
+        if(number > 1250){
+            setText('Eitaaa!')
+        }
+    }, [number])
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
-
+            <SectionTitle title="Exercícios #01"/>
             <div className="center">
                 <span className="text">{context.state.text}</span>
                 <span className="text">{context.state.number}</span>
@@ -29,6 +37,15 @@ const UseContext = (props) => {
             <div>
                 <button className="btn" onClick={() => addNumber(+1)}>+1</button>
                 <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+            </div>
+            <SectionTitle title="Exercícios #02"/>
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+                <div>
+                    <button className="btn" onClick={() => setNumber(number - 1)}>-1</button>
+                    <button className="btn" onClick={() => setNumber(number + 1)}>+1</button>
+                </div>
             </div>
         </div>
     )

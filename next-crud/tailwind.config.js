a0dc40c8d790c4c4a82e614878624804064535c9
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2022-11-04 19:45:04 -0300

    Componentes de botão e formulario

--- next-crud/tailwind.config.js
+++ next-crud/tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   "./src/components/**/*.{js,ts,jsx,tsx}",
   
   ],
+
+  safelist: [
+    'from-green-400',
+    'from-blue-400',
+    'from-gray-400',
+    'to-gray-700',
+    'to-blue-700',
+    'to-green-700'
+  ],
   
   theme: {
   

f01437cbf10f32b839d82b158ac79fbfba2e4b68
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2022-11-09 16:33:58 -0300

    Composição do layout

---
+++ todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <PageHeader name="Tarefas" small="Cadastro"/>
+                <TodoForm />
+                <TodoList />
+            </div>
+        )
+    }
+}
--- todo-app/frontend/webpack.config.js
+++ todo-app/frontend/webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
         filename: './app.js'
     },
     devServer: {
-        port: 8000,
-        contentBase: './public'
+        port: 8080,
+        contentBase: './public',
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
@@ -17,27 +17,24 @@ module.exports = {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
-            exclude: '/node_modules/',
+            exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },
-        {
-            test: '/\.css$/',
-            laoder: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },
-        {
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
-        }
-    ]
+        }]
     }
 }
